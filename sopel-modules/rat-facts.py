b0085cee502aed723cc230726d52a64d2b4f3266
Daniel Grace <thisgenericname@gmail.com>
2016-01-28 21:57:20 -0800

    Revert "Update rat-facts" commit, since it wasn't intended to be included on this branch.
    This reverts commit a6bbd30dac4a9772e0a9834955c0018395c487b6.

--- sopel-modules/rat-facts.py
+++ sopel-modules/rat-facts.py
@@ -10,58 +10,53 @@ http://sopel.chat/
 
 import json
 import os
-import re
 from sopel.module import commands, NOLIMIT, rule
 from sopel.config.types import StaticSection, ValidatedAttribute
-from sopel.tools import SopelMemory
-
 
 class RatfactsSection(StaticSection):
     filename = ValidatedAttribute('filename', str, default='')
-
 
 def configure(config):
     config.define_section('ratfacts', RatfactsSection)
     config.ratfacts.configure_setting('filename',
         "The name of the json file containing the fact list.")
 
-
-def setup(bot):
-    with open(bot.config.ratfacts.filename) as f:
-        facts = json.load(f)
-
-    if not isinstance(facts, dict):
-        # Something horribly wrong with the json
-        raise RuntimeError("expects rat-facts json to be a dict")
-
-    if 'ratbot' not in bot.memory:
-        bot.memory['ratbot'] = SopelMemory()
-    bot.memory['ratbot']['facts'] = facts
-
-
-@commands(r'[^\s]+')
+@rule('.*')
 def reciteFact(bot, trigger):
-    """Recite facts"""
-    facts = bot.memory['ratbot']['facts']
-    fact = trigger.group(1).lower()
-    if fact not in facts:
-        # Unknown fact
+    msgParts = trigger.group().split(' ')
+    cmd = msgParts[0]
+    if cmd.startswith('!'): #TODO use bot.config's command prefix
+        # Remove the !
+        cmd = cmd[1:]
+    else:
+        # We don't care about this message.
         return NOLIMIT
 
-    rats = trigger.group(2)
-    if rats:
-        # Reorganize the rat list for consistent & proper separation
-        # Split whitespace, comma, colon and semicolon (all common IRC multinick separators) then rejoin with commas
-        rats = ", ".join(filter(None, re.split(r"[,\s+]", rats))) or None
+    try:
+        with open(bot.config.ratfacts.filename) as f:
+            facts = json.load(f)
+    except IOError:
+        # We couldn't open facts.json, so act as if you're disabled.
+        return NOLIMIT
 
-    # reply_to automatically picks the sender's name if rats is None, no additional logic needed
-    return bot.reply(facts[fact], reply_to=rats)
-
+    if cmd in facts:
+        if len(msgParts) > 1:
+            # This command was directed at somebody.
+            return bot.say('%s: %s' % (msgParts[1], facts[cmd.lower()]))
+        else:
+            return bot.reply(facts[cmd.lower()])
+    else:
+        # Not one of our commands.
+        return NOLIMIT
 
 @commands('fact', 'facts')
 def listFacts(bot, trigger):
     """Lists the facts in the .JSON file"""
-    facts = bot.memory['ratbot']['facts']
-    if not facts:
-        return bot.reply("Like Jon Snow, I know nothing.  (Or there's a problem with the fact list.")
-    return bot.reply('Known facts: ' + ', '.join(sorted(facts.keys())))
+    try:
+        with open(bot.config.ratfacts.filename) as f:
+            facts = json.load(f)
+    except IOError:
+        # We couldn't open facts.json...
+        return bot.reply('There appears to be a problem with the fact list.')
+    return bot.reply('Known facts: '+', '.join(facts.keys()))
+
