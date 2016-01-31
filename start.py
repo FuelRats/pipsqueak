d564c57d9af4b28108a488e8922475fa1732f444
Daniel Grace <thisgenericname@gmail.com>
2016-01-30 17:13:36 -0800

    Add updated instructions and alternate start method to cope with sopel not putting the CWD in PYTHONPATH for module imports.

---
+++ start.py
@@ -0,0 +1,9 @@
+import sys
+import sopel.run_script
+
+if len(sys.argv) <= 1:
+    print("No configuration file specified.  Defaulting to sopel.cfg")
+    # This is a terrible, terrible idea ... but despite sopel.run_script() accepting an argv parameter, it does
+    # precisely nothing with it.
+    sys.argv = [sys.argv[0], '-c', 'sopel.cfg']
+sopel.run_script.main(sys.argv)
