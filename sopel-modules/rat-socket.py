3277e0668d425ae20ff7cdbe72c3008d1d5a28b2
Marenthyu <petersilie93@live.de>
2016-07-12 23:56:42 +0200

    WebSocket Listens further to the API Reacts to the following from RatTracker and prints appropriate messages to chat: fr+-, wr+-, sys+-, bc+-, inst+- and duty+- Only works if it's connected to the appropriate Websocket and API due to rescueIDs.

--- sopel-modules/rat-socket.py
+++ sopel-modules/rat-socket.py
@@ -11,6 +11,7 @@ http://sopel.chat/
 # Python imports
 import sys
 from threading import Thread
+import json
 
 #Sopel imports
 from sopel.formatting import bold, color, colors
@@ -30,6 +31,7 @@ log.startLogging(sys.stdout)
 #ratlib imports
 import ratlib.api.http
 urljoin = ratlib.api.http.urljoin
+
 
 
 import threading
@@ -64,7 +66,7 @@ def shutdown(bot=None):
 def setup(bot):
     ratlib.sopel.setup(bot)
     bot.memory['ratbot']['log'] = (threading.Lock(), collections.OrderedDict())
-    bot.memory['ratbot']['board'] = Socket()
+    bot.memory['ratbot']['socket'] = Socket()
 
     if not hasattr(bot.config, 'socket') or not bot.config.socket.websocketurl:
         websocketurl = '123'
@@ -82,6 +84,87 @@ def callapi(bot, method, uri, data=None, _fn=ratlib.api.http.call):
         return _fn(method, uri, data, log=bot.memory['ratbot']['apilog'], headers=headers)
 
 
+def removeTags(string):
+    try:
+        i = string.index('[')
+    except ValueError:
+        i = len(string)
+
+    return string[0:i]
+
+def getRatId(bot, ratname):
+    """
+    Gets the RatId for a given name from the API or 0 if it couldnt find anyone with that cmdrname
+    Args:
+        bot: the bot to pull the config from
+        ratname: the cmdrname to look for
+
+    Returns:
+
+    """
+    strippedname = removeTags(ratname)
+    try:
+        uri = '/rats?CMDRname=' + strippedname
+        result = callapi(bot=bot,method = 'GET',uri=uri)
+        # print(result)
+        data = result['data']
+        # print(data)
+        firstmatch = data[0]
+        id = firstmatch['_id']
+        return {'id':id, 'name':strippedname}
+    except IndexError as ex:
+        try:
+            #print('No rats with that CMDRname found. Trying nickname...')
+            uri = '/rats?nickname=' + strippedname
+            result = callapi(bot=bot, method='GET', uri=uri)
+            # print(result)
+            data = result['data']
+            # print(data)
+            firstmatch = data[0]
+            id = firstmatch['_id']
+            return {'id':id, 'name':strippedname}
+        except IndexError:
+            #print('no rats with that commandername or nickname found. trying gamertag...')
+            try:
+                uri = '/rats?gamertag=' + strippedname
+                result = callapi(bot=bot, method='GET', uri=uri)
+                # print(result)
+                data = result['data']
+                # print(data)
+                firstmatch = data[0]
+                id = firstmatch['_id']
+                return {'id':id, 'name':strippedname}
+            except IndexError:
+                #print('no rats with that commandername or nickname or gamertag found.')
+                return {'id':'0', 'name':strippedname, 'error':ex, 'description':'no rats with that commandername or nickname or gamertag found.'}
+    except ratlib.api.http.APIError as ex:
+        print('APIError: couldnt find RatId for '+strippedname)
+        return {'id':'0', 'name':strippedname, 'error':ex, 'description':'API Error while trying to fetch Rat'}
+
+def getRatName(bot, ratid):
+    result = callapi(bot=bot, method='GET', uri='/rats/'+ratid)
+    ret = 'unknown'
+    try:
+        data=result['data']
+        try:
+            ret = data['CMDRname']
+        except:
+            ret = data['nickname']
+    except:
+        ret = 'unknown'
+    # print('returning '+ret+' as name for '+ratid)
+    return ret
+
+def getClientName(bot, resId):
+    result = callapi(bot=bot, method='GET', uri='/rescues/' + resId)
+    ret = 'unknown'
+    try:
+        data = result['data']
+        ret = data['client']['nickname']
+    except:
+        ret = 'unknown'
+    return ret
+
 class Socket:
     def __enter__(self):
         return self._lock.__enter__()
@@ -90,31 +173,31 @@ class Socket:
         return self._lock.__exit__(exc_type, exc_val, exc_tb)
 
     def __init__(self):
+        self._lock = threading.RLock()
         print("Init for socket called!")
 
 
-@commands('sockettest','test','socket')
+@commands('reconnect')
 @ratlib.sopel.filter_output
 def sockettest(bot, trigger):
-    bot.say('Gotcha! - Testvalue is: ' + bot.config.socket.websocketurl)
+    bot.say('Sorry Pal, but you need to restart the bot to attempt a Manual Reconnect!')
 
 @commands('connectsocket','connect')
 @ratlib.sopel.filter_output
 def connectSocket(bot, trigger):
-    bot.say('gotcha, connecting.')
+    bot.say('Gotcha, connecting to the API\'s Websocket!')
+    MyClientProtocol.bot = bot
     factory = MyClientFactory(str(bot.config.socket.websocketurl) + ':' + bot.config.socket.websocketport)
     factory.protocol = MyClientProtocol
-    print('in connect')
+    # print('in connect')
     reactor.connectTCP(str(bot.config.socket.websocketurl).replace("ws://",''), int(bot.config.socket.websocketport), factory)
-    print('pls')
+    # print('pls')
     thread = Thread(target = reactor.run, kwargs={'installSignalHandlers':0})
     thread.start()
-    MyClientProtocol.bot = bot
-   # thread.join()
 
 
     #reactor.run(installSignalHandlers=0)
-    print('Im in?')
+    # print('Im in?')
 
 
 
@@ -122,7 +205,7 @@ class MyClientProtocol(WebSocketClientProtocol):
     bot = None
 
     def onOpen(self):
-        print('onOpen')
+        MyClientProtocol.bot.say('Successfully openend connection to Websocket!')
         self.sendMessage(str('{ "action":"stream:subscribe", "applicationId":"0xDEADBEEF" }').encode('utf-8'))
 
     def onMessage(self, payload, isBinary):
@@ -131,13 +214,78 @@ class MyClientProtocol(WebSocketClientProtocol):
 
       else:
          print("Text message received: {0}".format(payload.decode('utf8')))
-         MyClientProtocol.bot.say(payload.decode('utf8'))
+         handleWSMessage(payload)
+
 
     def onClose(self, wasClean, code, reason):
-        print('onclose')
-        MyClientProtocol.bot.say('Closed')
+        # print('onclose')
+        MyClientProtocol.bot.say('Closed connection with Websocket. Reason: '+str(reason))
 
 
+def handleWSMessage(payload):
+    response = json.loads(payload.decode('utf8'))
+    action = response['meta']['action']
+    data = response['data']
+    say = MyClientProtocol.bot.say
+    bot = MyClientProtocol.bot
+
+    def onduty(data):
+        #print('in function onduty!!!!!!!!')
+        if data['OnDuty'] == 'True':
+            say(str(getRatName(bot, data['RatID'])) + ' is now on Duty! (Current Location: '+data['currentSystem']+') [Reported by RatTracker]')
+        else:
+            say(str(getRatName(bot, data['RatID'])) + ' is now off Duty! [Reported by RatTracker]')
+
+    def welcome(data):
+        say('Successfully welcomed to Websocket!')
+
+    def fr(data):
+        client = getClientName(bot=bot, resId=data['RescueID'])
+        rat = getRatName(bot=bot, ratid=data['ratID'])
+        if data['FriendRequest'] == 'true':
+            say(rat + ': fr+ [Case ' + client + ', RatTracker]')
+        else:
+            say(rat + ': fr- [Case ' + client + ', RatTracker]')
+
+    def wr(data):
+        client = getClientName(bot=bot, resId=data['RescueID'])
+        rat = getRatName(bot=bot, ratid=data['ratID'])
+        if data['WingRequest'] == 'true':
+            say(rat + ': wr+ [Case ' + client + ', RatTracker]')
+        else:
+            say(rat + ': wr- [Case ' + client + ', RatTracker]')
+
+    def system(data):
+        client = getClientName(bot=bot, resId=data['RescueID'])
+        rat = getRatName(bot=bot, ratid=data['ratID'])
+        if data['ArrivedSystem'] == 'true':
+            say(rat + ': sys+ [Case ' + client + ', RatTracker]')
+        else:
+            say(rat + ': sys- [Case ' + client + ', RatTracker]')
+
+    def bc(data):
+        client = getClientName(bot=bot, resId=data['RescueID'])
+        rat = getRatName(bot=bot, ratid=data['ratID'])
+        if data['BeaconSpotted'] == 'true':
+            say(rat + ': bc+ [Case ' + client + ', RatTracker]')
+        else:
+            say(rat + ': bc- [Case ' + client + ', RatTracker]')
+
+    def inst(data):
+        client = getClientName(bot=bot, resId=data['RescueID'])
+        rat = getRatName(bot=bot, ratid=data['ratID'])
+        if data['InstanceSuccessful'] == 'true':
+            say(rat + ': inst+ [Case ' + client + ', RatTracker]')
+        else:
+            say(rat + ': inst- [Case ' + client + ', RatTracker]')
+
+    wsevents = {"OnDuty:update":onduty,'welcome':welcome,'FriendRequest:update':fr,'WingRequest:update':wr,'SysArrived:update':system,'BeaconSpotted:update':bc,'InstanceSuccessful:update':inst}
+    #print('keys of wsevents: '+str(wsevents.keys()))
+    #print(action)
+
+    if action in wsevents.keys():
+        #print('Action is in wskeys!!')
+        wsevents[action](data=data)
 
 
 class MyClientFactory(ReconnectingClientFactory, WebSocketClientFactory):
