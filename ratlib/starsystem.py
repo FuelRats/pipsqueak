FuelRats/pipsqueak

commit fc7a48040e9fcbfe959a43da7e0fe92af0a16665

Fix getting ratnames only working when the commander name somewhat matched the IRC name and add status code checking for database refresh and abort when it is not 200

ratlib/starsystem.py
@@ -93,10 +93,10 @@ def _refresh_database(bot, force=False, callback=None, background=False, db=None
     :param db: Database handle
     """
     start = time()
-    # print('Starting refresh at '+str(start))
+    print('Starting refresh at '+str(start))
     edsm_url = bot.config.ratbot.edsm_url or "http://edsm.net/api-v1/systems?coords=1"
     status = get_status(db)
-    # print('status: '+str(status))
+    print('status: '+str(status))
     edsm_maxage = float(bot.config.ratbot.edsm_maxage) or 60*12*12
     if not (
         force or
@@ -104,21 +104,24 @@ def _refresh_database(bot, force=False, callback=None, background=False, db=None
         (datetime.datetime.now(tz=datetime.timezone.utc) - status.starsystem_refreshed).total_seconds() > edsm_maxage
     ):
         # No refresh needed.
-        # print('not force and no refresh needed')
+        print('not force and no refresh needed')
         return False
 
     if callback:
         callback()
 
     if background:
-        # print('background!')
+        print('Sending edsm refresh to background!')
         return bot.memory['ratbot']['executor'].submit(
             _refresh_database, bot, force=True, callback=None, background=False
         )
 
     fetch_start = time()
     print('Started Database refresh......')
-    data = requests.get(edsm_url).json()
+    req = requests.get(edsm_url)
+    if req.status_code != 200:
+        print('ERROR When calling EDSM - Status code was '+str(req.status_code))
+        return
     print('Fetch done!')
     fetch_end = time()
     # with open('run/systems.json') as f:

ratlib/api/names.py
@@ -71,14 +71,15 @@ def getRatId(bot, ratname, platform=None):
                     rat = {'id':cmdr, 'platform':ratplat, 'name':ratnam}
                     if rat['platform'] == platform:
                         id = rat['id']
-                        ret = {'id':id, 'name': ratnam, 'platform':platform}
+                        ret = {'id':cmdr, 'name': ratnam, 'platform':platform}
                         returnlist.append(ret)
             strippedname = removeTags(ratname)
             for retelement in returnlist:
                  if (retelement['name']==ratname) or (retelement['name']==strippedname) or (retelement['name']==strippedname.replace('_', ' ')):
                     ret = retelement
-        savedratids.update({ratname: ret})
-        savedratnames.update({id: {'name': ratnam, 'platform': ret['platform'], 'id':ret['id']}})
+        if ret != {'id':None, 'name':None, 'platform':None}:
+            savedratids.update({ratname: ret})
+            savedratnames.update({id: {'name': ratnam, 'platform': ret['platform'], 'id':ret['id']}})
         return ret
     except:
             # print('Calling fallback on ratID search as no rat with registered nickname '+strippedname+' or '+ratname+' was found.')

commit 440fd06a14fbc6920dd979961073ea9fa7ec4a3a

Fix error introduced with https://github.com/FuelRats/pipsqueak/commit/fc7a48040e9fcbfe959a43da7e0fe92af0a16665#diff-004051e123dbab25cba779c07c2f7484L121

ratlib/starsystem.py
@@ -125,6 +125,7 @@ def _refresh_database(bot, force=False, callback=None, background=False, db=None
     if req.status_code != 200:
         print('ERROR When calling EDSM - Status code was '+str(req.status_code))
         return
+    data = req.json()
     print('Fetch done!')
     fetch_end = time()
     # with open('run/systems.json') as f: