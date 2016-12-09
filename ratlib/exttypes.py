e3a72e9b62f0a7adf1acf0761414fce35ac315f4
Daniel Grace <Daniel Grace>
2016-12-08 21:59:02 -0800

    Remove debugging code for the SQLPoint type.

--- ratlib/exttypes.py
+++ ratlib/exttypes.py
@@ -37,29 +37,21 @@ class SQLPoint(types.UserDefinedType):
 
     def bind_processor(self, dialect):
         def process(value):
-            print("In bind_processor: value is {!r}".format(value))
             if value is None:
                 return value
             if None in value:
                 raise ValueError('Value cannot contain None values')
-            result = "" + ",".join(str(x) for x in value) + ""
-            print(result)
-            return result
+            return ",".join(str(x) for x in value)
         return process
 
     def result_processor(self, dialect, coltype):
         def process(value):
-            print("In result_processor: value is {!r}".format(value))
             if value is None:
                 return value
             return Point(self.number_type(x) for x in self._re_pattern.match(value).groups())
         return process
 
     def bind_expression(self, bindvalue):
-        print("In bind_expression: bindvalue is {!r}".format(bindvalue))
         if bindvalue.value is None:
             return None
-        # result = "POINT(" + ",".join(str(x) for x in bindvalue.value) + ")"
-        result = sql.func.point(bindvalue, type_=self)
-        # print("...returned {}".format(str(result)))
-        return result
+        return sql.func.point(bindvalue, type_=self)
