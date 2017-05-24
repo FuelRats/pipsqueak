b14e2d74a1987f643d5d7bf4baeffc754262733c
Tivec <henrik.bergvin@gmail.com>
2017-05-24 15:39:13 +0200

    Added a !twitterdebug command for techrats Moved message building to a separate function Refuse to send messages if the case has no system (None value) Append the last 6 of the message ID for case tweets. Feedback if the tweet failed because it is duplicate.

--- sopel-modules/rat-twitter.py
+++ sopel-modules/rat-twitter.py
@@ -23,7 +23,7 @@ from sopel.module import commands
 from twitter import TwitterError
 
 import ratlib.sopel
-from ratlib.api.names import require_rat
+from ratlib.api.names import require_rat, require_techrat
 from ratlib.db import with_session, Starsystem
 from ratlib.sopel import parameterize
 
@@ -82,11 +82,24 @@ def setup(bot):
         return
 
     bot.memory['ratbot']['twitterapi'] = api
-
+    bot.memory['ratbot']['twitterdebug'] = False
 
 # Convenience function
 def requires_case(fn):
     return parameterize('r', "<client or case number>")(fn)
+
+@commands('tweetdebug')
+@require_techrat
+def cmd_tweetdebug(bot, trigger):
+    """
+    Toggles debug mode on and off. Does not save when the bot is reloaded.
+    In debug mode, tweets are not sent. Debug mode defaults to False.    
+    """
+    debug = bot.memory['ratbot']['twitterdebug']
+    debug = not debug
+    bot.memory['ratbot']['twitterdebug'] = debug
+    bot.reply('Done. Twitter module is now {}in debug mode.'.format('not ' if not debug else ''))
+
 
 @commands('tweet')
 @parameterize("t", usage="<text to tweet>")
@@ -98,6 +111,8 @@ def cmd_tweet(bot, trigger, line):
     Required parameter: The message you want to send, max 140 characters.
     """
     api = bot.memory['ratbot']['twitterapi']
+    debug = bot.memory['ratbot']['twitterdebug']
+
     if not api:
         bot.reply("The Twitter interface is not correctly configured. Unable to continue.")
         return
@@ -121,19 +136,68 @@ def cmd_tweet(bot, trigger, line):
                 return
             pass
 
+    if debug:
+        bot.say('Tweet debug: "{}"'.format(line))
+        return
+
     try:
         api.PostUpdate(line)
-    except TwitterError:
-        bot.say('Tweet failed. Please try again in a moment and speak to your friendly rat technicians.')
+    except TwitterError as twitterError:
+        if 'code' in twitterError.message[0]:
+            if twitterError.message[0]['code'] == 187:
+                bot.say(
+                    "Tweet failed! The tweet is considered a duplicate! Try a different message.")
+        else:
+            bot.say('Tweet failed. Please try again in a moment and speak to your friendly rat technicians.')
+
+        import traceback
+        traceback.print_exc()
         return
 
     bot.say('Tweet "{}" sent!'.format(line))
     pass
 
 
+def get_tweet_for_case(rescue, db):
 
+    def lookup_system(name, model=Starsystem):
+        if name is not None:
+            return db.query(model).filter(model.name_lower == name.lower()).first()
+        return None
 
-@commands('tweetc','tweetcase')
+    platform = rescue.platform.upper()
+    cr = "CR " if rescue.codeRed else ""
+
+    # this is the base message
+    message = "[{platform}] Rats needed for {cr}rescue!".format(platform=platform, cr=cr)
+
+    if db is None:
+        return message
+
+    starsystem = lookup_system(rescue.system)
+    if starsystem:
+        landmark, distance = starsystem.nearest_landmark(db, True)
+
+        # we couldn't calculate a distance (system not in eddb, probably)
+        if distance is None:
+            return message
+
+        if (starsystem.name_lower == landmark.name_lower) or distance < 50:
+            return "[{platform}] Rats needed for {cr}rescue near {system}!".format(platform=platform, cr=cr, system=landmark.name)
+
+        # let's work on distances
+        if distance < 250:
+            dist = '{dist}ly'.format(dist=math.ceil(distance / 10) * 10)
+        elif distance < 1000:
+            dist = '{dist}ly'.format(dist=math.ceil(distance / 100) * 100)
+        else:
+            dist = '{dist}kly'.format(dist=math.ceil(distance / 1000))
+
+        return "[{platform}] Rats needed for {cr}rescue {distance} from {system}!".format(platform=platform, cr=cr, distance=dist, system=landmark.name)
+
+    return message
+
+@commands('tweetcase','tweetc')
 @parameterize('r', usage='<client or case number>')
 @with_session
 @require_rat('Sorry, you need to be a registered and drilled Rat to use this command.')
@@ -141,18 +205,15 @@ def cmd_tweetc(bot, trigger, rescue, db = None):
     """
     Send a tweet based on a case, using generic terms (in bubble, near landmark).
     Required parameter: Client name or board index.
+    
+    Aliases: tweetcase, tweetc
     """
     api = bot.memory['ratbot']['twitterapi']
+    debug = bot.memory['ratbot']['twitterdebug']
 
     if not api:
         bot.reply("The Twitter interface is not correctly configured. Unable to continue.")
         return
-
-    def lookup_system(name, model=Starsystem):
-        if name is not None:
-            return db.query(model).filter(model.name_lower == name.lower()).first()
-        return None
-
 
     platform = rescue.platform
 
@@ -160,44 +221,41 @@ def cmd_tweetc(bot, trigger, rescue, db = None):
         bot.say('The case platform is unknown. Please set it with the corresponding command and try again.')
         return
 
-    platform = platform.upper()
-    starsystem = lookup_system(rescue.system)
-    cr = "CR " if rescue.codeRed else ""
+    if rescue.system is None:
+        bot.say('The case has no assigned system. Please do this before sending a tweet.')
+        return
 
-    message = "[{platform}] Rats needed for {cr}rescue!".format(platform=platform, cr=cr)
-
-    if starsystem:
-        landmark, distance = starsystem.nearest_landmark(db, True)
-
-        # is it in the bubble?
-        if (landmark.name_lower == "sol" or landmark.name_lower == "fuelum") and distance < 500:
-            message = "[{platform}] Rats needed for {cr}rescue in the bubble!".format(platform=platform, cr=cr, system=starsystem.name)
-
-        # is it near or at a landmark?
-        elif (starsystem.name_lower == landmark.name_lower) or distance < 500:
-            message = "[{platform}] Rats needed for {cr}rescue near {system}!".format(platform=platform, cr=cr, system=landmark.name)
-
-        # ok, just give us the distance
-        else:
-            if distance < 1000:
-                dist = '{dist}ly'.format(dist=math.ceil(distance / 100) * 100)
-            else:
-                dist = '{dist}kly'.format(dist=math.ceil(distance / 1000))
-            message = "[{platform}] Rats needed for {cr}rescue {distance} from {system}!".format(platform=platform, cr=cr, distance=dist, system=landmark.name)
+    message = get_tweet_for_case(rescue, db)
 
     if not message:
         bot.say('An unknown error occurred. Speak with your local Tech Rats')
         return
 
-    if len(message) < 115:
+    if len(message) < 100:
         message = "{msg} Call your jumps, Rats!".format(msg=message)
-    elif len(message) > 140:
+
+    if rescue.id is not None:
+        message = '{msg} -case {id}'.format(msg=message, id=rescue.id[-6:])
+
+    if len(message) > 140:
         bot.say('Tweet failed! Message is too long ({} characters). Please report this to your local Tech Rats'.format(len(message)))
+
+    if debug:
+        bot.say('Tweet debug: "' + message + '"')
+        return
 
     try:
         api.PostUpdate(message)
-    except TwitterError:
-        bot.say('Tweet failed. Please try again in a moment and speak to your friendly rat technicians.')
+    except TwitterError as twitterError:
+        if 'code' in twitterError.message[0]:
+            if twitterError.message[0]['code'] == 187:
+                bot.say("Tweet failed! The tweet is considered a duplicate! Either you have already sent one, or something went wrong. Contact your nearest Tech Rat.")
+        else:
+            bot.say('Tweet failed. Please try again in a moment and speak to your friendly rat technicians.')
+
+        bot.say('Failed tweet: "' + message + '"')
+        import traceback
+        traceback.print_exc()
         return
 
     bot.say('Tweet sent: "' + message + '"')
