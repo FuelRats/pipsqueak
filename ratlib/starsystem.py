1678817e5903358e460b99821ba87d8183fd678b
Cameron Welter <camjwelt@gmail.com>
2019-12-10 01:35:31 +0000

    don't format inputted system name in query, make error objects consistent

--- ratlib/starsystem.py
+++ ratlib/starsystem.py
@@ -397,33 +397,32 @@ def refresh_bloom(bot, db):
     return bloom
 
 def sysapi_query(system, querytype):
-    system = system.title()
     if querytype == "search":
         try:
             response = requests.get('https://system.api.fuelrats.com/search?name={}'.format(system))
             if response.status_code != 200:
-                return {"error": "System API did not respond with valid data."}
+                return { "meta": { "error": "System API did not respond with valid data." } }
             result = response.json()
         except Timeout:
-            return {"error": "The request to Systems API timed out!"}
+            return { "meta": { "error": "The request to Systems API timed out!"} }
         return result
     if querytype == "landmark":
         try:
             response = requests.get('https://system.api.fuelrats.com/landmark?name={}'.format(system))
             if response.status_code != 200:
-                return {"error": "System API did not respond with valid data."}
+                return { "meta": { "error": "System API did not respond with valid data."} }
             result = response.json()
         except Timeout:
-            return {"error": "The request to Systems API timed out!"}
+            return { "meta": { "error": "The request to Systems API timed out!"} }
         return result
     else:
         try:
             response = requests.get(f'https://system.api.fuelrats.com/api/systems?filter[name:eq]={system}')
             if response.status_code != 200:
-                return {"error": "System API did not respond with valid data."}
+                return { "meta": { "error": "System API did not respond with valid data."} }
             result = response.json()['data']
         except Timeout:
-            return {"error": "The request to Systems API timed out!"}
+            return { "meta": { "error": "The request to Systems API timed out!"} }
         return result
 
 def scan_for_systems(bot, line, min_ratio=0.05, min_length=6):
